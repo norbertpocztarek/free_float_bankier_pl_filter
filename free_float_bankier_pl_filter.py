459aedc45e0722edfa1a8b5035e006f00d9f81ee
Norbert <norbert.pocztarek@gmail.com>
2021-07-10 18:19:08 +0200

    v. 0.0.1
    0.0.1
    Added:
    - getting website code
    - searching for "Free float" text - it's give me number of position
    - printing value of Free float

--- free_float_bankier_pl_filter.py
+++ free_float_bankier_pl_filter.py
@@ -1,5 +1,11 @@
 import requests
 
 r = requests.get('https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=ARTERIA')
-
-print(r.text)
+free_float = r.text
+x = free_float
+new = free_float.find("Free float")
+print()
+print()
+print("Free float for ARTERIA is: " + x[new+19:new+25])
+print()
+#print(r.text)
